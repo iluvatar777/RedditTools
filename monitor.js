fda609fca16a3baabadffa0b7ab91b5b4abf8a1f
= <matt.giesmann@gmail.com>
2017-05-03 17:25:33 -0400

    Remove promise-queue code and module

--- monitor.js
+++ monitor.js
@@ -6,34 +6,9 @@ const pageRetriever = require("./pageRetriever.js");
 const db = require("./dbhandler.js");
 
 const Promise = require('bluebird');
-//const Queue = require('promise-queue');
 const cheerio = require('cheerio');
 const arrayUnion = require('array-union');
 
-
-/*let postQueue = new Queue(10, Infinity);
-
-
-let queueMonitor = '';
-let queueMonitorCount = 0;
-
-// called on interval.
-const checkQueues = function() {
-	queueMonitorCount += 1 ;
-	const level = (queueMonitorCount % 60 == 0) ? 'info' : 'silly';
-
-	const cpQ = postQueue.getQueueLength();
-	const cpP = postQueue.getPendingLength();
-
-	logger.log(level, 'QueueMonitor ' + queueMonitorCount + ': postQueue ' + cpP + '/' + cpQ);
-
-	if (cpQ + cpP == 0) {
-		logger.info('Queues are empty, stopping queue monitor');
-		clearInterval(queueMonitor);
-
-		db.closeConnectionPool();			// TODO connection should not be managed here
-	}
-};*/
 
 // given a subreddit, retrieves list of hot, new, and [TODO] monitored post fullnames
 const retrieveSubr = function(subr) {
@@ -50,10 +25,6 @@ const retrieveSubr = function(subr) {
 // given a post fulllname, retrieves, processes and INSERTs to db
 // uses a queue to manage concurrency
 const processPost = function(subr, fullname) {
-	/*return postQueue.add(function() {
-		logger.verbose('Adding to postQueue: ' + subr + '/' + fullname);
-		return pageRetriever.getPostPage(subr, fullname); 
-	})*/
 	return pageRetriever.getPostPage(subr, fullname)
 	.then(function($post) {
 		const postObj = processor.processPostPage($post);
@@ -76,14 +47,7 @@ const processSub = function(subr) {
 	db.initConnectionPool();			// TODO connection should not be managed here
 
 	return retrieveSubr(subr)
-	/*.then(function(posts) {
-		for (let i = 0; i < posts.length; i++) {								//TODO replace with .map() with concurrency
-			processPost(subr, posts[i])
-		};
-	});*/
 	.map(function(post) {
-		//logger.info('Starting Queue Monitor');				// TODO this is real bad
-		//queueMonitor = setInterval(checkQueues, 1000);
 		return processPost(subr, post);
 	}, {concurrency : 15}
 	)
