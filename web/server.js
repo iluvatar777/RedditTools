ce4fc99977432eed30aba7b5f501d54c32d5e462
= <matt.giesmann@gmail.com>
2017-05-27 16:57:45 -0400

    view simple chart with series selection

--- web/server.js
+++ web/server.js
@@ -12,8 +12,42 @@ const server = restify.createServer({
 	version: '1.0.0'
 });
 server.use(restify.plugins.acceptParser(server.acceptable));
+server.pre(restify.pre.sanitizePath());
 //server.use(restify.plugins.queryParser());
 //server.use(restify.plugins.bodyParser());
+
+
+const postQuery = function(req, res, next) {
+	const sql = '	SELECT p.fullname, p.subreddit,\
+					COUNT(p.fullname) AS dataPoints\
+					FROM postdata d\
+					JOIN post p\
+					ON d.fullname = p.fullname\
+					GROUP BY p.fullname\
+					HAVING dataPoints >= ?\
+					ORDER BY p.subreddit, datapoints DESC;';
+	logger.debug('get posts');
+
+	return db.query(sql, [req.params.minCount], req.params.minCount)
+	.then(function(data) {
+
+		let datapoints = [];
+
+		for (let i = 0; i < data.rows.length; i++) {
+			datapoints.push({fullname : data.rows[i].fullname, subreddit : data.rows[i].subreddit, count : data.rows[i].dataPoints});
+		}
+
+		res.send(JSON.stringify(datapoints));
+	})
+	.catch(function(err) {
+		logger.warn(err);
+	})
+	.finally(function() {
+		logger.debug('posts retrieved from db, sending to client');
+		return next();
+
+	})
+};
 
 
 server.get('/echo/:name', function (req, res, next) {
@@ -23,7 +57,7 @@ server.get('/echo/:name', function (req, res, next) {
 
 server.get('/api/postdata/:fullname', function (req, res, next) {
 	const sql = 'SELECT * FROM postdata WHERE fullname = ?';
-	logger.warn('get postdata for ' + req.params.fullname);
+	logger.debug('get postdata for ' + req.params.fullname);
 
 	return db.query(sql, [req.params.fullname], req.params.fullname)
 	.then(function(data) {
@@ -42,14 +76,21 @@ server.get('/api/postdata/:fullname', function (req, res, next) {
 		res.send(JSON.stringify(dataset));
 	})
 	.catch(function(err) {
-		logger.warn(err);
+		logger.error(err);
 	})
 	.finally(function() {
-		logger.warn('finally');
+		logger.debug('postdata' + req.params.fullname + 'retrieved from db, sending to client');
 		return next();
 
 	})
 });
+
+server.get('/api/posts', function (req, res, next) {
+	req.params.minCount = 50;
+	postQuery(req, res, next);
+});
+server.get('/api/posts/:minCount', postQuery);
+
 
 server.get(/\/?.*/, restify.plugins.serveStatic({
 	directory: __dirname + '/static',
@@ -61,4 +102,3 @@ server.listen(config.port, function () {
 	logger.info('Web Server listening at ' + server.url);
 	logger.info('dir: ' + __dirname + '/static');
 });
-
--- web/static/index.js
+++ web/static/index.js
@@ -1,27 +1,46 @@
 "use strict"
 
-$(document).ready(function() {
-    $.ajax({
-        url: "/api/postdata/t3_6bm55g"
+const loadChart = function(fullname) {
+	return $.ajax({
+        url: "/api/postdata/" + fullname
     }).then(function(data) {
+
+    	console.log('series data recieved');
     	const ctx = 'myChart';
     	var scatterChart = new Chart(ctx, {
-	    type: 'line',
-	    data: {
-	        datasets: $.parseJSON(data)
-	    },
-	    options: {
-	        scales: {
-	            xAxes: [{
-	                type: 'linear',
-	                position: 'bottom'
-	            }]
-	        }
-	    }
-	});
-
-
-
-       consol.log(data);
+		    type: 'line',
+		    data: {
+		        datasets: $.parseJSON(data)
+		    },
+		    options: {
+		        scales: {
+		            xAxes: [{
+		                type: 'linear',
+		                position: 'bottom'
+		            }]
+		        },
+		        responsive: false
+		    }
+		});
+       console.log(data);
     });
+};
+
+$(document).ready(function() {
+
+    $.ajax({
+        url: "/api/posts/60"
+   	}).then(function(data) {
+    	console.log('series recieved');
+
+    	data = $.parseJSON(data);
+    	console.log(data);
+		for (let i = 0; i < data.length; i++) {
+			console.log(data[i]);
+			$('#series').append($('<option>', {value: data[i].fullname, text: data[i].subreddit + ' ' + data[i].fullname + ' (' + data[i].count + ')'}));
+		}
+    });
+
+   	loadChart('t3_65gs9n');
+
 });
